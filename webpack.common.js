1f56d60733aed6bf7ad90b1987e004f3768369e7
AdiL18 <adi.l@comstar.co.il>
2021-11-27 22:41:28 +0200

    fix package name

--- webpack.common.js
+++ webpack.common.js
@@ -8,7 +8,7 @@ module.exports = ()=>{
         entry: path.join(__dirname, "src", "index.js"),
         output: {
           filename: `${pkg.name}.js`,
-          library: pkg.name.charAt(0).toUpperCase() + pkg.name.slice(1),
+          library: "Datepicker",
           path:path.resolve(__dirname, "dist"),
           globalObject: '(typeof self !== \'undefined\' ? self : this)', // TODO Hack (for Webpack 4+) to enable create UMD build which can be required by Node without throwing error for window being undefined (https://github.com/webpack/webpack/issues/6522)
           umdNamedDefine: true,
